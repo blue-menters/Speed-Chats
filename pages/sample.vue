7b20d3ee5592760009f19e6fce179c58ef4f0cfc
Satoshi Ono <s14228so@sfc.keio.ac.jp>
2019-07-06 17:30:46 +0900

    Cloud functions 入れた

---
+++ pages/sample.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="getDate">クリック</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    getData() {}
+  }
+};
+</script>
+
+<style>
+</style>
